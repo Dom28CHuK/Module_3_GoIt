4b956438e4ea045ff40f221d0f836e25539d4017
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-10 08:42:53 +0200

    add class SaveStarShip

---
+++ Java/SaveStarShip.java
@@ -0,0 +1,9 @@
+public class SaveStarShip {
+    //Test output
+    public static void main(String[] args) {
+        SaveStarShip ship = new SaveStarShip();
+
+        //Should be 10
+        System.out.println(ship.calculateDistance(-10));
+    }
+}
